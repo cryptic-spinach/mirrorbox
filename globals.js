367df775f63bcbabc1c87dab674ba303c177cc75
Chase Reynolds <chase@Chases-MBP.home>
2021-09-08 03:11:39 -0400

    Change Verbiage

--- globals.js
+++ globals.js
@@ -43,4 +43,4 @@ let eyeHeight = 40;
 let successVector;
 
 
-let messageText = "Use the interactive to determine how many paths exist between the object and observer for any given mirror length. The slider below can be used to adjust the angle of incidence and the vertical slider controls the mirror length. Note: The path of the light must make contact with the white dot above the eye in order to be considered a valid path."
+let messageText = "Use the interactive to determine how many paths exist between the object and observer for any given mirror length. The slider above can be used to adjust the angle of incidence and the vertical slider controls the mirror length. Note: The path of the light must make contact with the white dot above the eye in order to be considered a valid path."
